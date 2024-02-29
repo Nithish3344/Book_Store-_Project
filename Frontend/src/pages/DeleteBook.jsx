a195d93b279fedbd884ba77682ded70b971ba18d
Nithish3344 <kondle_n@cy.iitr.ac.in>
2024-02-29 15:23:56 +0530

    Final deploy

--- Frontend/src/pages/DeleteBook.jsx
+++ Frontend/src/pages/DeleteBook.jsx
@@ -14,7 +14,7 @@ const DeleteBook = () => {
   const handleDeleteBook = () => {
     setLoading(true);
     axios
-      .delete(`https://book-store-project-6pk6.onrender.com/books${id}`)
+      .delete(`https://book-store-project-6pk6.onrender.com/books/${id}`)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book Deleted successfully', { variant: 'success' });
